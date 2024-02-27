f6bf3b76a61fecefc31c369c90cdac67468cd1ef
TheMumblingMammoth <45206030+TheMumblingMammoth@users.noreply.github.com>
2024-02-27 23:45:05 +0300

    Update comments

--- EyeTrackFatigue/Input/__init__.py
+++ EyeTrackFatigue/Input/__init__.py
@@ -1,5 +1,25 @@
-from DeviceManager.DeviceHandler import DeviceHandler
+# input init file
+# модуль загрузки данных в систему
+# сюда относится всё, связанное с считыванием и форматированием входных данных
 
-# dv = DeviceHandler('../test_data.csv')
-# data = dv.request_data()
-# print(data)
+
+from .InputSection import InputSection
+from ..DeviceManager import DeviceHandler, Synchronizer
+
+
+def read_video_file(path):
+    # считывание файла
+    return InputSection()
+
+
+def read_csv_file(path):
+    # считывание файла
+    device = DeviceHandler(path)
+    data = InputSection()
+    data.add_data_array(device.request_data())
+    return data
+
+def init_reading_stream(path):
+    # запуск потока ввода
+    sync = Synchronizer(path)
+    #sync.start_stream()
