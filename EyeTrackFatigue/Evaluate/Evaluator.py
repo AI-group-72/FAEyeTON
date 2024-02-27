4e71d02120f200b6723b03d99fb92074a605bdef
TheMumblingMammoth <45206030+TheMumblingMammoth@users.noreply.github.com>
2024-02-28 00:49:21 +0300

    Update comments

--- EyeTrackFatigue/Evaluate/Evaluator.py
+++ EyeTrackFatigue/Evaluate/Evaluator.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+# абстрактный класс, задающий основную логику для классов описывающих различные модели оценки
 class Evaluator(ABC):
     @abstractmethod
     def evaluate(self, data):
