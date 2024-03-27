271a4dc3effb0f5457f5f8dd16761fafd30a2718
TheMumblingMammoth <45206030+TheMumblingMammoth@users.noreply.github.com>
2024-03-27 16:28:28 +0300

    Update setup.py

--- Meta/setup.py
+++ Meta/setup.py
@@ -2,61 +2,34 @@ from setuptools import find_packages, setup
 
 
 def get_long_description():
-    with open("README.md") as file:
-        return file.read()
+    return 'https://github.com/AI-group-72/FAEyeTON/blob/main/README.md'
 
+def get_requirements():
+    return ['pandas', 'openpyxl', 'scikit-learn', 'numpy', 'PyQt6', 'opencv-python', 'joblib']
 
 setup(
     name="EyeTrackFatigue",
-    version="0.2.1",
-    description="EyeTrackFatigue description",
+    version="1.0.14",
+    description="проект FAEyeTON - Библиотека открытого кода для оценки функционального состояния утомления оператора на основе динамической активности взгляда и головы. Проект Код-ИИ-2022.",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
-    author="...",
+    author="Кашевник А.М. Кузнецов В.В. Брак И.В. Мамонов А.А. Коваленко С.Д.",
     author_email="anton.mamonov.golohvastogo@mail.ru",
     url="https://github.com/AI-group-72/FAEyeTON",
     project_urls={
         "GitHub Project": "https://github.com/AI-group-72/FAEyeTON",
         "Issue Tracker": "https://github.com/AI-group-72/FAEyeTON/Meta/issues",
     },
-    packages=find_packages(
-        include=["DeviceManager", "DeviceManager.*",
-                 "Input", "Input.*",
-                 "Analise", "Analise.*",
-                 "Emulate", "Emulate.*",
-                 "Evaluate", "Evaluate.*",
-                 "UI.DataGather"],
-    ),
+    packages=['EyeTrackFatigue.DeviceManager', 'EyeTrackFatigue.Input',
+              'EyeTrackFatigue.Analise', 'EyeTrackFatigue.Evaluate', 'EyeTrackFatigue.UI', 'EyeTrackFatigue.Meta'],
+    install_requires=get_requirements(),
+    python_requires=">=3.12",
 
     keywords=[
         "EyeTrackFatigue",
     ],
-    license="MIT",
+    license="GNU LGPL",
 )
 
 
-'''
-package_data={
-    "EyeTrackFatigue_client": ["data/*.cfg"],
-    },
-    install_requires=[
-        "requests==2.27.1",
-    ],
-    setup_requires=[
-        "pytest-runner",
-        "flake8==4.0.1",
-    ],
-    tests_require=[
-        "pytest==7.1.2",
-        "requests-mock==1.9.3",
-    ],
-    python_requires=">=3.6",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-'''
+
